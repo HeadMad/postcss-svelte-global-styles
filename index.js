eb1b33966ce960286778fed9e4f482807f723bfa
HeadMad <kov.ps@ya.ru>
2020-02-09 04:13:15 +0800

    fix brackets

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const postcss = require('postcss')
 const isGlobal = /:global\(\s*\S+\s*\)/
 
 module.exports = postcss.plugin('postcss-svelte-global-styles', () => {
-  return (root) => {
+  return root => {
     root.walkRules(rule => {
       let selectors = rule.selector.split(',')
       let globalSelectors = selectors.map(sel => {
+        // change selector
         return isGlobal.test(sel) ? sel : ':global(' + sel.trim() + ')'
-      }
+      })
       rule.selector = globalSelectors.join(',')
     })
-
   }
 })
