9546aef81892e9bed9a93d45a8cb2acc2ab2e8ad
HeadMad <kov.ps@ya.ru>
2020-04-07 02:29:18 +0800

    fix global animation

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const postcss = require('postcss')
-const isGlobal = /:global\(\s*\S+\s*\)/
+const isGlobal = /:global\((.+?)\)/
 
 module.exports = postcss.plugin('postcss-svelte-global-styles', () => {
   return root => {
-    root.walkRules(rule => {
+    root.walkRules((rule) => {
       const globalSelectors = postcss.list
         .comma(rule.selector)
-        .map(sel => !isGlobal.test(sel)
-          ? ':global(' + sel.trim() + ')'
+        .map((sel) => !isGlobal.test(sel)
+          ? ':global(' + sel + ')'
           : sel)
       rule.selector = globalSelectors.join(',')
+    })
+
+    root.walkAtRules('keyframes', (atrule) => {
+      if (postcss.vendor.prefix(atrule.params) !== '-global-')
+        atrule.params = '-global-' + atrule.params
+      atrule.walkRules(isGlobal, (rule) => {
+        rule.selector = rule.selector.replace(isGlobal, '$1')
+      })
     })
   }
 })
